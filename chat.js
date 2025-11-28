e7394b4cc34020a5e4a2a703ba0acdeb113e0c0a
EnBergGold <118936643+EnBergGold@users.noreply.github.com>
2025-11-28 11:47:19 +0300

    Update chat.js

--- chat.js
+++ chat.js
@@ -506,8 +506,8 @@ class DeepSeekChat {
           <rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect>
           <path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path>
         </svg>
-      </button>
-      <button class="message-forward-btn" data-message-id="${messageId}">` : ''}
+      </button>` : ''}
+      <button class="message-forward-btn" data-message-id="${messageId}">
         <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
           <path d="M4 12v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-8"/>
           <polyline points="16 6 12 2 8 6"/>
